a20f465bb24f083021cb7b54baedf806c3821806
kimamovic21 <imamovic.kerim@gmail.com>
2022-10-06 16:33:00 +0200

    izmjene
    folder 10

---
+++ 10_model/script.js
@@ -0,0 +1,21 @@
+// Variables
+
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+// Event listeners
+
+openBtn.addEventListener('click', () => {
+    modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
